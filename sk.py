174592c1b7a52f21dd7ad794743c554f39e86f7b
Ubuntu <ubuntu@ip-172-31-45-158.ec2.internal>
2018-10-27 22:26:58 +0000

    add two files

---
+++ sk.py
@@ -0,0 +1,16 @@
+import numpy as np
+from sklearn.svm import SVC
+from pymongo import MongoClient
+l = []
+client = MongoClient()
+db = client.training_database
+collection = db.traning_collection
+X = np.asarray()
+posts = db.posts
+for post in posts.find(): 
+     t.append(post['data']) 
+X = np.asarray(t)    
+y = np.array([1,1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,2])
+clf = SVC(gamma='auto')
+clf.fit(X, y)
+clf.predict([[]])
